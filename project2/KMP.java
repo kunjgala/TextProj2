1eeedc8660ffde9d8a5447d6a92c29b91986a22f
Kunj Gala <kunjgala15@gmail.com>
2023-04-29 03:28:48 -0700

    second commit

--- project2/KMP.java
+++ project2/KMP.java
@@ -1,16 +1,50 @@
 package project2;
+import java.util.*;
 
 public class KMP {
   
   public int match(String T, String P) {
-    int[] arr = new int[P.length()];
-    arr[0] = 0;
+    int[] failfn = ComputeFailFn(P);
+    int i =0;
     int j =0;
-    int i=1;
-    while(i<P.length()) {
-      
+    while(i<T.length()) {
+      if(T.charAt(i)==P.charAt(j)) {
+        if(j==P.length()-1) {
+          return i-j;
+        }
+        else {
+          i++;
+          j++;
+        }
+      }
+      else {
+        if(j>0) j=failfn[j-1];
+        else i++;
+      }
     }
     return -1;
+  }
+
+  public static int[] ComputeFailFn(String P) {
+    int[] failfn = new int[P.length()];
+    failfn[0] = 0;
+    int i =1;
+    int j =0;
+    while(i<P.length()) {
+      if(P.charAt(i)==P.charAt(j)) {
+        failfn[i]=j+1;
+        i++;
+        j++;
+      }
+      else if(j>0) {
+        j = failfn[j-1];
+      }
+      else {
+        failfn[i]=0;
+        i++;
+      }
+    }
+    return failfn;
   }
 
 }
